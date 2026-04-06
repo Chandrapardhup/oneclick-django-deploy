2773bad61a15e5f254f46e7afd24ed839e60633d
Chandrapardhu <chandrapardhup@gmail.com>
2026-04-06 12:21:56 +0530

    Fix DB host for CI

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ COPY . .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-EXPOSE 8000
+EXPOSE 8005
 
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8005"]
--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-sn)&18&f&6re^p%dx24$tjm((nsp!uemlwpk@9+aw81)8d+_=(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
